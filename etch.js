bc00288b1c0b25a44491638a151dc718f823a854
BrentBachelder <brentbachelder@gmail.com>
2022-05-20 22:09:44 -0700

    Completed Canvas Image and Drawing bugs...
    The next commit I'm going to create an image change menu and give user
    the ability to change pixel size.
    
    I'm also going to try to upload it to a server to see if I can store the
    pictures in the same network.

--- etch.js
+++ etch.js
@@ -1,15 +1,26 @@
 const grid = document.getElementById("grid-container");
-var gridSize = 512;
-var gridHeight = 512;
-var gridWidth = 384;
-var gridPixelsPerUnit = 8;
-
-var mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+const gridHeight = 512;
+const gridWidth = 384;
+let gridPixelsPerUnit = 16;
 
 let prevX = -1;
 let prevY = -1;
+var gridCreated = false;
+
+let totalDrawnBlocks = 0;
+let correctDrawnBlocks= 0;
+
+var mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => { mouseDown = false; prevX = -1; prevY = -1; }
+
+var img = new Image();
+img.crossOrigin = 'anonymous';
+img.src = 'https://i.ibb.co/DgPH8WG/test-picture.jpg';
+
+var canvas = document.getElementById('canvas');
+canvas.height = 512;
+var ctx = canvas.getContext('2d');
 
 function createGrid() {
     grid.style.width = gridWidth + "px";
@@ -29,6 +40,7 @@ function createGrid() {
             grid.appendChild(box);
         }
     }
+    gridCreated = true;
 }
 
 function drawOnBox(e) {
@@ -48,17 +60,53 @@ function drawOnBox(e) {
         for(var i = 1; i < totalSteps; i++) {
             newX = prevX + Math.round((xSteps / totalSteps) * i) * Math.sign(curX - prevX);
             newY = prevY + Math.round((ySteps / totalSteps) * i) * Math.sign(curY - prevY);
-            console.log(newX);
-            document.getElementById(newX + "-" + newY).classList.add('block');
+            document.getElementById(newX + "-" + newY).classList.add('black');
         }
     }
     
-    document.getElementById(id).classList.add('block');
+    document.getElementById(id).classList.add('black');
     prevX = curX;
     prevY = curY;
 }
 
-window.onload = () => {
+var getCanvasBoxes = function() {
+	ctx.drawImage(img, 0, 0);
+	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	const data = imageData.data;
+    let newLine = 4 * (gridWidth - (gridPixelsPerUnit * 1.5)) * gridPixelsPerUnit;
+    let startI = (4 * gridWidth * (gridPixelsPerUnit / 2)) + (4 * (gridPixelsPerUnit / 2));
+    
+    let xPos = 0;
+    let yPos = 0;
+	for (var i = startI; i < data.length - 4; i += 4 * gridPixelsPerUnit) {
+        if(xPos == gridWidth / gridPixelsPerUnit) { yPos++; xPos = 0; i += newLine; }
+        let dataAverage = (data[i] + data[i + 1] + data[1 + 2]) / 3;
+        if(dataAverage < 100 && document.getElementById(xPos + "-" + yPos)) {
+            document.getElementById(xPos + "-" + yPos).classList.add('gray');
+        }
+        xPos++;
+	}
+	ctx.putImageData(imageData, 0, 0);
+};
+
+function changeImage() {
+    clearDrawing();
+    clearImage();
+    img.src = "https://i.ibb.co/mzWPNss/Untitled-1.jpg";
+    img.onload = getCanvasBoxes;
+}
+
+function clearImage() {
+    var grays = document.querySelectorAll('.gray');
+    for(var i = 0; i < grays.length; i++) grays[i].classList.remove('gray');
+}
+
+function clearDrawing() {
+    var blacks = document.querySelectorAll('.black');
+    for(var i = 0; i < blacks.length; i++) blacks[i].classList.remove('black');
+}
+
+img.onload = function() {
     createGrid();
-    DrawCanvas();
+    getCanvasBoxes();
 }
